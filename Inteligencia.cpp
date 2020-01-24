db1b69e134decf0242a4676432eb9789b181affd
Saúl Páez <saulpaez03mail.com>
2020-01-23 19:40:50 -0430

    Añadida a implementación de la lista y los dos tipos de nodos. Los datos son leídos y almacenados en listas, respectivamente. Lista de espera y lista de pisos)prioridad añadidas.

--- Inteligencia.cpp
+++ Inteligencia.cpp
@@ -3,38 +3,95 @@
 #include <fstream>
 using namespace std;
 struct Persona{
-	string Nombre, Apellido, Origen, Destino;
+	string nombre, apellido, origen,destino;
 };
-struct NodoP{
-	Persona Info;
-	Nodo *Next, *Prev;
-	Nodo(Persona A){
-		Info.Nombre = A.Nombre;
-		Info.Apellido = A.Apellido;
-		Info.Destino = A.Destino;
-		Info.Origen= A.Origen;
+struct Nodo{// Declaracion del nodo que contiene la informacion de las personas;
+	Persona info;
+	Nodo *next, *prev;
+	Nodo(Persona A){//Constructor del nodo;
+		info.nombre = A.nombre;
+		info.apellido = A.apellido;
+		info.destino = A.destino;
+		info.origen= A.origen;
 	}
-	
+
 };
-class ListaP{
-	NodoP *Begin;
+struct NodoPiso{//Declaracion del nodod que contiene las solicitudes de movimiento del ascensor;
+	string piso;
+	NodoPiso *next,*prev;
+	NodoPiso(string A){//Constructor del nodo;
+		piso=A;
+		next=NULL;
+		prev=NULL;
+	}
+};
+template <class T>
+ class Lista{//Implementacion de la lista, utilizando T como template para que funcione con ambos tipos de nodo.
+	public:
+	T *first, *last;
+	int size;
+	Lista(){
+		first= NULL;
+		last= NULL;
+		size=0;
+	}
+	void InsertFront(T *A){//Inserta un elemento al principio de la lista;
+		if(size!=0){
+			A->next= first;
+			A->prev= NULL;
+			first->prev= A;
+			first=A;
+		}else{
+			first=A;
+			last=A;
+		}
+		size++;
+	}
+	void InsertBack(T *A){//Inserta un elemento al final de la lista;
+		if(size!=0){
+			last->next=A;
+			A->prev=last;
+			last=A;
+		}else{
+			first=A;
+			last=A;
+		}
+		size++;
+	}
+	void erase(T *A){//Borra un element de la lista;
+		A->prev->next=A->next;
+		A->next->prev=A->prev;
+		delete (A);
+		size --;
+	}
 	
 };
 
 int main() {
-	int Max,num;
+	int max,num,count=1;
 	char symbol;
-	Persona A;
-	ifstream Entrada;
-	Entrada.open("Entrada.in",ios::in);
-	Entrada>>Max;
+	Persona *A;
+	A= new Persona;
+	Lista<NodoPiso> Pisos=Lista<NodoPiso>();
+	Lista<Nodo> Ascensor=Lista<Nodo>();
+	Nodo *aux;
+	NodoPiso *aux1;
+	ifstream entrada;
+	entrada.open("Entrada.in",ios::in);
+	entrada>>max;
 	while(!entrada.eof()){
-		cin>>symbol;
+		Persona A;
+		entrada>>symbol;
 		if(symbol=='+'){
-			Entrada<<num;
+			entrada>>num;
 			for(int i=0;i<num;i++){
-				Entrada>>A.Nombre>>A.Apellido>>A.Origen>>A.Destino;
+				entrada>>A.nombre>>A.apellido>>A.origen>>A.destino;
+				aux= new Nodo(A);
+				Ascensor.InsertBack(aux);
+				aux1=new NodoPiso(A.origen);
+				Pisos.InsertBack(aux1);
 			}
+			count++;
 		}
 	}
 	return 0;
